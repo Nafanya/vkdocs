b70c84e824b26d7b00d6a4678abec621a69eff26
pva701 <pva701@gmail.com>
2016-02-28 03:44:14 +0300

    tabs and drawer init via material design nav drawer library

---
+++ app/src/main/java/io/github/nafanya/vkdocs/DownloadedFragments.java
@@ -0,0 +1,7 @@
+package io.github.nafanya.vkdocs;
+
+/**
+ * Created by pva701 on 28.02.16.
+ */
+public class DownloadedFragments {
+}
--- app/src/main/java/io/github/nafanya/vkdocs/DummyFragment.java
+++
@@ -1,6 +0,0 @@
-package io.github.nafanya.vkdocs;
-
-import android.app.Fragment;
-
-public class DummyFragment extends Fragment {
-}
