00f1bf3688638e6fd0eb32c791e3c26104a75795
pva701 <pva701@gmail.com>
2016-03-11 02:11:50 +0300

    update gradle plugin. after ff <3

---
+++ app/src/main/java/io/github/nafanya/vkdocs/presentation/ui/views/OpenProgressDialog.java
@@ -0,0 +1,24 @@
+package io.github.nafanya.vkdocs.presentation.ui.views;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+
+import io.github.nafanya.vkdocs.R;
+
+
+public class OpenProgressDialog extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.open_progress_dialog, null));
+        return builder.create();
+    }
+}
