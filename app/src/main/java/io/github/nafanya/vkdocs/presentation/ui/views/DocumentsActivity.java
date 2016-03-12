4ceb86676942e86db1516b8f8e306d6182f6c403
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2016-03-12 22:00:08 +0300

    Fixed styles for spinner

--- app/src/main/java/io/github/nafanya/vkdocs/presentation/ui/views/DocumentsActivity.java
+++ app/src/main/java/io/github/nafanya/vkdocs/presentation/ui/views/DocumentsActivity.java
@@ -3,10 +3,20 @@ package io.github.nafanya.vkdocs.presentation.ui.views;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import io.github.nafanya.vkdocs.R;
 
-public class DocumentsActivity extends AppCompatActivity {
+public class DocumentsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.toolbar_spinner) Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +24,32 @@ public class DocumentsActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_documents);
 
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                getSupportActionBar().getThemedContext(),
+                R.array.doc_type_filter_items, R.layout.appbar_filter_title);
+        adapter.setDropDownViewResource(R.layout.appbar_filter_list);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
         Fragment fragment = DocumentsFragment.newInstance(null);
 
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragment_container, fragment)
                 .commit();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/io/github/nafanya/vkdocs/presentation/ui/views/DocumentsFragment.java
+++ app/src/main/java/io/github/nafanya/vkdocs/presentation/ui/views/DocumentsFragment.java
@@ -3,11 +3,13 @@ package io.github.nafanya.vkdocs.presentation.ui.views;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import java.util.List;
 
