258bf8b4e0c103807dd0a78eb5c90643ccc8e901
pva701 <pva701@gmail.com>
2016-03-21 01:41:53 +0300

    audio player in progress

--- app/src/main/java/io/github/nafanya/vkdocs/presentation/ui/AudioPlayer.java
+++ app/src/main/java/io/github/nafanya/vkdocs/presentation/ui/AudioPlayer.java
@@ -5,8 +5,6 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 
-import java.io.IOException;
-
 /**
  * Created by pva701 on 21.03.16.
  */
@@ -15,20 +13,21 @@ public class AudioPlayer {
     private MediaPlayer player;
 
     public AudioPlayer(Context app, String uri) {
-        //Uri playUri = Uri.parse("file:///sdcard/Songs/ARR Hits/hosannatamil.mp3");
+        player = new MediaPlayer();
         Uri playUri = Uri.parse(uri);
         player.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
         try {
             player.setDataSource(app, playUri);
-        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            player.prepare();
+        } catch (Exception e) {
+            e.printStackTrace();
             //Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-        } catch (SecurityException e) {
-            //Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-        } catch (IllegalStateException e) {
-            //Toast.makeText(getApplicationContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
-        } catch (IOException e) {
-            //e.printStackTrace();
         }
     }
 
@@ -40,16 +39,8 @@ public class AudioPlayer {
     }
 
     public void play() {
-        stop();
-
-        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                stop();
-            }
-        });
+        player.setOnCompletionListener(mp -> stop());
 
         player.start();
     }
-
 }
--- app/src/main/java/io/github/nafanya/vkdocs/presentation/ui/views/fragments/MusicPlayFragment.java
+++ app/src/main/java/io/github/nafanya/vkdocs/presentation/ui/views/fragments/MusicPlayFragment.java
@@ -1,6 +1,5 @@
 package io.github.nafanya.vkdocs.presentation.ui.views.fragments;
 
-import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,20 +7,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import io.github.nafanya.vkdocs.domain.model.VkDocument;
 import io.github.nafanya.vkdocs.presentation.ui.AudioPlayer;
 
 public class MusicPlayFragment extends Fragment {
-    private AudioPlayer player;
+    public static String MUSIC_KEY = "music_key";
 
-    public static MusicPlayFragment newInstance() {
-        return null;
+    private VkDocument audioDocument;
+    private AudioPlayer player;
+    public static MusicPlayFragment newInstance(VkDocument document) {
+        MusicPlayFragment fragment = new MusicPlayFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(MUSIC_KEY, document);
+        fragment.setArguments(bundle);
+        return fragment;
     }
 
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        player = new AudioPlayer(getActivity().getApplication(), "");
+        audioDocument = getArguments().getParcelable(MUSIC_KEY);
+        if (audioDocument.getPath() != null)
+            player = new AudioPlayer(getActivity().getApplication(), audioDocument.getPath());
+        else
+            player = new AudioPlayer(getActivity().getApplication(), audioDocument.url);
         setRetainInstance(true);
     }
 
